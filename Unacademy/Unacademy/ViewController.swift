144d2caf8709d867b064bc9c1a6e866a193dfe2b
Subhodip Banerjee <riju@Subhodips-MacBook-Pro.local>
2017-03-06 14:05:02 +0530

    Second commit

---
+++ Unacademy/Unacademy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Unacademy
+//
+//  Created by  Subhodip Banerjee on 06/03/17.
+//  Copyright © 2017 Subhodip Banerjee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
